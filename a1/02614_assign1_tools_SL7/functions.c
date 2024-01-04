7a8a12cd892ba0b84f42d6012bd1927952afb038
Andreas Lau Hansen <s194235@student.dtu.dk>
2024-01-04 10:40:45 +0100

    add some functions

---
+++ a1/02614_assign1_tools_SL7/functions.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <cblas.h>
+
+void matmult_lib(int m, int n, int k, double **A, double **B, double **C) {
+    double alpha = 1.0;
+    double beta = 0.0;
+    int lda = k;
+    int ldb = n;
+    int ldc = n;
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, A[0], lda, B[0], ldb, beta, C[0], ldc);
+    }
+
+void matmult_nat(int m, int n, int k, double **A, double **B, double **C) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++){
+            C[i][j] = 0.0;
+            for (int l = 0; l < k; l++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
+void matmult_mnk(int m, int n, int k, double **A, double **B, double **C) {
+    for (int i = 0; i < m; i++) {
+        for (int l = 0; l < k; l++) {
+            C[i][0] = 0.0;
+            for (int j = 0; j < n; j++){
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
+void matmult_nmk(int m, int n, int k, double **A, double **B, double **C) {
+    for (int j = 0; j < n; j++){
+        for (int i = 0; i < m; i++) {
+            C[i][j] = 0.0;
+            for (int l = 0; l < k; l++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
+void matmult_kmn(int m, int n, int k, double **A, double **B, double **C) {
+    for (int l = 0; l < k; l++) {
+        for (int i = 0; i < m; i++) {
+            C[i][0] = 0.0;
+            for (int j = 0; j < n; j++){
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
+void matmult_knm(int m, int n, int k, double **A, double **B, double **C) {
+    for (int l = 0; l < k; l++) {
+        for (int j = 0; j < n; j++){
+            for (int i = 0; i < m; i++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
+void matmult_mkn(int m, int n, int k, double **A, double **B, double **C) {
+    for (int i = 0; i < m; i++) {
+        for (int l = 0; l < k; l++) {
+            C[i][0] = 0.0;
+            for (int j = 0; j < n; j++){
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
+void matmult_nkm(int m, int n, int k, double **A, double **B, double **C) {
+    for (int j = 0; j < n; j++){
+        for (int l = 0; l < k; l++) {
+            for (int i = 0; i < m; i++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
