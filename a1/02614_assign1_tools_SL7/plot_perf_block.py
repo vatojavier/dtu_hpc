6c46c2e9ed52bdb7b8966fc8cc9bc692008e94b5
Javi <vatojavier@gmail.com>
2024-01-05 11:20:30 +0100

    Plot for blocking

--- a1/02614_assign1_tools_SL7/plot_perf_block.py
+++ a1/02614_assign1_tools_SL7/plot_perf_block.py
@@ -1,23 +1,35 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.ticker import ScalarFormatter
 
 # Plot block size vs. performance
 df = pd.read_csv(
-    "data/mm_g4_bs_2000m19859234.out",
+    "data/mm_g4_bs_2048_m19860602.out",
     sep="\s+",
     header=None,
     names=["Memory (kB)", "Mflops/s", "#", "type", "block_size"],
 )
 df.drop("#", axis=1, inplace=True)
 
+df["block_footprint"] = [(3 * 8 * x * x)/1024 for x in df["block_size"]]
 
-# PLot block size vs. performance
+# Plot block size vs. performance
 fig, ax = plt.subplots(figsize=(10, 6))
 
-ax.plot(df["block_size"], df["Mflops/s"], marker="o")
+# ax.plot(df["block_footprint"], df["Mflops/s"], marker="o")
+ax.semilogx(df["block_footprint"], df["Mflops/s"], base=2, marker='o', linestyle='dashed')
+ax.vlines(
+    [32, 256,  30720],
+    df["Mflops/s"].min(),
+    df["Mflops/s"].max(),
+    colors="r",
+    linestyles="dashed",
+    label="L1, L2, L3",
+)
 
-ax.set_xlabel("Block size")
+ax.set_xlabel("Block footprint (kB)")
 ax.set_ylabel("Mflops/s")
-ax.set_title("Matrix multiplication performance")
-plt.savefig("data/perf_block.pdf", bbox_inches="tight")
+ax.xaxis.set_major_formatter(ScalarFormatter())
+ax.set_title("Matrix multiplication performance using blocking")
+plt.savefig("data/perf_block_footprint.pdf", bbox_inches="tight")
 plt.show()
