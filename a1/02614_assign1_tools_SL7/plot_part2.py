644950a0a09e790690dd85e2fed7b4d158fa9481
Andreas Lau Hansen <andreasLauH@outlook.com>
2024-01-05 16:52:13 +0100

    plotting for part 2

--- a1/02614_assign1_tools_SL7/plot_part2.py
+++ a1/02614_assign1_tools_SL7/plot_part2.py
@@ -19,7 +19,7 @@ df['type'] = df['type'].apply(lambda x: x[-3:])
 # Plot
 fig, ax = plt.subplots(figsize=(10, 6))
 for key, grp in df.groupby(['type']):
-    ax.semilogx(grp['Memory (kB)'], grp['Mflops/s'], label=key[0], base=2, marker='o', linestyle='dashed')
+    ax.semilogx(grp['Memory (kB)'][1:], grp['Mflops/s'][1:], label=key[0], base=2, marker='o', linestyle='dashed')
 ax.vlines([32, 256, 30720], df['Mflops/s'].min(), df['Mflops/s'].max(), colors='r', linestyles='dashed', label='L1, L2, L3')
 ax.set_xlabel('Memory (kB)')
 ax.set_ylabel('Mflops/s')
@@ -40,7 +40,7 @@ df['type'] = df['type'].apply(lambda x: x[-3:])
 # Plot
 fig, ax = plt.subplots(figsize=(10, 6))
 for key, grp in df.groupby(['type']):
-    ax.semilogx(grp['Memory (kB)'], grp['Mflops/s'], label=key[0], base=2, marker='o', linestyle='dashed')
+    ax.semilogx(grp['Memory (kB)'][1:], grp['Mflops/s'][1:], label=key[0], base=2, marker='o', linestyle='dashed')
 ax.vlines([32, 256, 30720], df['Mflops/s'].min(), df['Mflops/s'].max(), colors='r', linestyles='dashed', label='L1, L2, L3')
 ax.set_xlabel('Memory (kB)')
 ax.set_ylabel('Mflops/s')
@@ -60,7 +60,7 @@ df['type'] = df['type'].apply(lambda x: x[-3:])
 # Plot
 fig, ax = plt.subplots(figsize=(10, 6))
 for key, grp in df.groupby(['type']):
-    ax.semilogx(grp['Memory (kB)'], grp['Mflops/s'], label=key[0], base=2, marker='o', linestyle='dashed')
+    ax.semilogx(grp['Memory (kB)'][1:], grp['Mflops/s'][1:], label=key[0], base=2, marker='o', linestyle='dashed')
 ax.vlines([32, 256, 30720], df['Mflops/s'].min(), df['Mflops/s'].max(), colors='r', linestyles='dashed', label='L1, L2, L3')
 ax.set_xlabel('Memory (kB)')
 ax.set_ylabel('Mflops/s')
@@ -90,7 +90,7 @@ plt.show()
 df = pd.read_csv('opt/opt_exps_all_orderings.out', sep='\s+', header=None, names=['Memory (kB)', 'Mflops/s','err', '#', 'type'])
 df.drop('#', axis=1, inplace=True)
 df.drop('err', axis=1, inplace=True)
-
+df['type'] = df['type'].apply(lambda x: x[-3:])
 # Plot
 fig, ax = plt.subplots(figsize=(10, 6))
 for key, grp in df.groupby(['type']):
