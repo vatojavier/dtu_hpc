23861534a3930f5a44ecd74b8a10226f278ba974
Javi <vatojavier@gmail.com>
2024-01-09 14:34:46 +0100

    format

--- a2/alloc3d.c
+++ a2/alloc3d.c
@@ -1,29 +1,35 @@
 #include <stdlib.h>
 
 double ***
-malloc_3d(int m, int n, int k) {
+malloc_3d(int m, int n, int k)
+{
 
     if (m <= 0 || n <= 0 || k <= 0)
         return NULL;
 
-    double ***p = (double***) malloc(m * sizeof(double **) +
+    double ***p = (double ***)malloc(m * sizeof(double **) +
                                      m * n * sizeof(double *));
-    if (p == NULL) {
+    if (p == NULL)
+    {
         return NULL;
     }
 
-    for(int i = 0; i < m; i++) {
-        p[i] = (double **) p + m + i * n ;
+    for (int i = 0; i < m; i++)
+    {
+        p[i] = (double **)p + m + i * n;
     }
 
-    double *a = (double*) malloc(m * n * k * sizeof(double));
-    if (a == NULL) {
-	free(p);
-	return NULL;
+    double *a = (double *)malloc(m * n * k * sizeof(double));
+    if (a == NULL)
+    {
+        free(p);
+        return NULL;
     }
 
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             p[i][j] = a + (i * n * k) + (j * k);
         }
     }
@@ -31,8 +37,8 @@ malloc_3d(int m, int n, int k) {
     return p;
 }
 
-void
-free_3d(double ***p) {
+void free_3d(double ***p)
+{
     free(p[0][0]);
     free(p);
 }
--- a2/main.c
+++ a2/main.c
@@ -16,66 +16,68 @@
 
 #define N_DEFAULT 100
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    int 	N = N_DEFAULT;
-    int 	iter_max = 1000;
-    double	tolerance;
-    double	start_T;
-    int		output_type = 0;
-    char	*output_prefix = "poisson_res";
-    char        *output_ext    = "";
-    char	output_filename[FILENAME_MAX];
-    double 	***u = NULL;
-
+    int N = N_DEFAULT;
+    int iter_max = 1000;
+    double tolerance;
+    double start_T;
+    int output_type = 0;
+    char *output_prefix = "poisson_res";
+    char *output_ext = "";
+    char output_filename[FILENAME_MAX];
+    double ***u = NULL;
 
     /* get the paramters from the command line */
-    N         = atoi(argv[1]);	// grid size
-    iter_max  = atoi(argv[2]);  // max. no. of iterations
-    tolerance = atof(argv[3]);  // tolerance
-    start_T   = atof(argv[4]);  // start T for all inner grid points
-    if (argc == 6) {
-	output_type = atoi(argv[5]);  // ouput type
+    N = atoi(argv[1]);         // grid size
+    iter_max = atoi(argv[2]);  // max. no. of iterations
+    tolerance = atof(argv[3]); // tolerance
+    start_T = atof(argv[4]);   // start T for all inner grid points
+    if (argc == 6)
+    {
+        output_type = atoi(argv[5]); // ouput type
     }
 
     // allocate memory
-    if ( (u = malloc_3d(N, N, N)) == NULL ) {
+    if ((u = malloc_3d(N, N, N)) == NULL)
+    {
         perror("array u: allocation failed");
         exit(-1);
     }
 
     /*
      *
-     * fill in your code here 
+     * fill in your code here
      *
      *
      */
 
-    // dump  results if wanted 
-    switch(output_type) {
-	case 0:
-	    // no output at all
-	    break;
-	case 3:
-	    output_ext = ".bin";
-	    sprintf(output_filename, "%s_%d%s", output_prefix, N, output_ext);
-	    fprintf(stderr, "Write binary dump to %s: ", output_filename);
-	    print_binary(output_filename, N, u);
-	    break;
-	case 4:
-	    output_ext = ".vtk";
-	    sprintf(output_filename, "%s_%d%s", output_prefix, N, output_ext);
-	    fprintf(stderr, "Write VTK file to %s: ", output_filename);
-	    print_vtk(output_filename, N, u);
-	    break;
-	default:
-	    fprintf(stderr, "Non-supported output type!\n");
-	    break;
+    // dump  results if wanted
+    switch (output_type)
+    {
+    case 0:
+        // no output at all
+        break;
+    case 3:
+        output_ext = ".bin";
+        sprintf(output_filename, "%s_%d%s", output_prefix, N, output_ext);
+        fprintf(stderr, "Write binary dump to %s: ", output_filename);
+        print_binary(output_filename, N, u);
+        break;
+    case 4:
+        output_ext = ".vtk";
+        sprintf(output_filename, "%s_%d%s", output_prefix, N, output_ext);
+        fprintf(stderr, "Write VTK file to %s: ", output_filename);
+        print_vtk(output_filename, N, u);
+        break;
+    default:
+        fprintf(stderr, "Non-supported output type!\n");
+        break;
     }
 
     // de-allocate memory
     free_3d(u);
 
-    return(0);
+    return (0);
 }
