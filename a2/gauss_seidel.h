c95f257f4225910b3807fbc7d16759cd92d9bcb5
Javi <vatojavier@gmail.com>
2024-01-11 13:22:32 +0100

    Gauss parallel but slower

--- a2/gauss_seidel.h
+++ a2/gauss_seidel.h
@@ -7,6 +7,8 @@
 // define your function prototype here
 int gauss_seidel_seq(double ***, double ***, int , int , double );
 
+int gauss_seidel_omp_wrong(double ***, double ***, int , int , double );
+
 int gauss_seidel_omp(double ***, double ***, int , int , double );
 
 #endif
